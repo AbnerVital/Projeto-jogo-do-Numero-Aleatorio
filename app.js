1fb93e3cf6e9284f9c523072ace6b6c6261616cb
Abner Vital <avgchaves@gmail.com>
2025-01-29 22:34:56 -0300

    finalizando o modulo de FUNÇÕES

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-// let titulo = document.querySelector('h1');
-// titulo.innerHTML = 'Jogo do Número secreto';
-
-// let paragrafo = document.querySelector('p');
-// paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+let numeroSecreto = gerarNumeroAleatorio();
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -11,5 +7,15 @@ function exibirTextoNaTela(tag, texto) {
 
 exibirTextoNaTela('h1', 'Jogo do Número Secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+function verificarChute(){
+    let chute = document.querySelector('input').value;
+    console.log(chute ==numeroSecreto);
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+
+}
 
 
